6e7f2d50acb8450a9cc4491441bc2ec71ac3c1fd
Vanessa Santos <98504873+VanessaSotnas@users.noreply.github.com>
2022-02-03 17:47:40 -0300

    calculadora

--- Main.java
+++ Main.java
@@ -16,9 +16,9 @@ public class Main {
 	sc.nextLine();
 	System.out.println("Escolha qual operacao voce deseja fazer:");
 	System.out.print("[1]Adicao | [2]Subtracao | [3]Divisao | [4]Multiplicacao ");
+		
 	int valorOperacao = sc.nextInt();
 
-	
 	if (valorOperacao == 1) {
 		resultado = valor1 + valor2;
 		System.out.println(valor1 + " + " + valor2 + " = " + resultado);
@@ -39,8 +39,6 @@ public class Main {
 		System.out.println("Valor digitado invalido. Tente novamente.");
 	}
 	   
-	
-	 
 	
 	sc.close();
 			
